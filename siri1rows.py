10a8a3316691d3a305db26491c861fdccc4f1673
sirivarshini728 <sirivarshiniy@gmail.com>
2025-04-12 14:19:41 +0530

    Add files via upload

---
+++ siri1rows.py
@@ -0,0 +1,25 @@
+#python program to print numbers in triangular form using for loop
+rows=int(input("enter number of rows:"))
+num= 1
+for i in range(1,rows+1):
+ for j in range(i):
+  print(num,"",end="")
+ num +=1
+ print("\n")
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
